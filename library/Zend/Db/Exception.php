4b353d66889de3cb12d9fe4ca4e120ad9df45c77
Wouter Samaey <wouter.samaey@storefront.be>
2022-02-24 12:55:51 +0100

    Zend_Db_Exception now extends RuntimeException instead of Zend_Exception which did not exist anymore

--- library/Zend/Db/Exception.php
+++ library/Zend/Db/Exception.php
@@ -20,16 +20,11 @@
  */
 
 /**
- * Zend_Exception
- */
-// require_once 'Zend/Exception.php';
-
-/**
  * @category   Zend
  * @package    Zend_Db
  * @copyright  Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class Zend_Db_Exception extends Zend_Exception
+class Zend_Db_Exception extends RuntimeException
 {
 }
